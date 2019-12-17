60d059ab65e1048ccfefb4df519355b86175f757
andreydashch <andreydashch@gmail.com>
2019-12-17 22:34:06 +0200

    zero, bigger zero, multiply

--- src/main/java/streams/StreamExercises.java
+++ src/main/java/streams/StreamExercises.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamExercises {
-    final int[] array = {1, 3 , 4, 5, 6, 1, 2};
+    final int[] array = {1, 3 , 4, 5, 6, 1, 2, 0, 0, -1};
     List<Integer> list;
 
     StreamExercises() {
@@ -18,8 +18,11 @@ public class StreamExercises {
     public static void main(String[] args) {
         StreamExercises main = new StreamExercises();
 
-        main.print(main.firstWithAverage().toString());
-        main.print(main.firstWithoutAverage().toString());
+        main.print(main.firstWithAverage());
+        main.print(main.firstWithoutAverage());
+        main.print(main.countZeroElements());
+        main.print(main.countElementBiggerZero());
+        main.print(main.multiplyElements(2));
     }
 
     Double firstWithAverage() {
@@ -30,12 +33,29 @@ public class StreamExercises {
     }
 
     Double firstWithoutAverage() {
-        return list.stream()
-                .mapToDouble(x -> x)
-                .reduce(0.0, Double::sum) / list.size();
+        return (double) list.stream()
+                .reduce(0, Integer::sum) / list.size();
     }
 
-    void print(String text) {
-        System.out.println(text);
+    Long countZeroElements() {
+        return list.stream()
+                .filter(x -> x.equals(0))
+                .count();
+    }
+
+    Long countElementBiggerZero() {
+        return list.stream()
+                .filter(x -> x > 0)
+                .count();
+    }
+
+    List<Integer> multiplyElements(int multiplier) {
+        return list.stream()
+                .map(x -> x * multiplier)
+                .collect(Collectors.toList());
+    }
+
+    void print(Object value) {
+        System.out.println(value.toString());
     }
 }
