63551f56b06dfcfebc8eff8f7b5cbdfa3e926bdd
andreydashch <andreydashch@gmail.com>
2019-12-17 16:28:14 +0200

    Array list implements Simple

--- src/main/java/simplecollections/arraylist/ArrayIterator.java
+++ src/main/java/simplecollections/arraylist/ArrayIterator.java
@@ -6,6 +6,10 @@ public class ArrayIterator<E> implements Iterator<E> {
     private int index = 0;
     E[] values;
 
+    public ArrayIterator(E[] values) {
+        this.values = values;
+    }
+
     public boolean hasNext() {
         return index < values.length;
     }
--- src/main/java/simplecollections/arraylist/ArrayList.java
+++ src/main/java/simplecollections/arraylist/ArrayList.java
@@ -1,4 +1,37 @@
 package simplecollections.arraylist;
 
-public class ArrayList {
+import simplecollections.Simple;
+
+public class ArrayList<E> implements Simple<E> {
+    public boolean add(E e) {
+        return false;
+    }
+
+    public void delete(int index) {
+
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void update(int index, E e) {
+
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public E next() {
+        return null;
+    }
+
+    public void remove() {
+        // Noting
+    }
 }
